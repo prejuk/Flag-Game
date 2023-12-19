bdb47cf25d87f4c9d5fdb102fbc0e5de27c24c61
prejuk <93695329+prejuk@users.noreply.github.com>
2023-12-18 20:28:38 -0800

    Add files via upload

---
+++ GuessTheFlagApp/GuessTheFlagApp/GuessTheFlagAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagAppApp.swift
+//  GuessTheFlagApp
+//
+//  Created by Preju Kanuparthy on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
